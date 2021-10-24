626e3486b011148fdcf54367d4318027639e096b
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2021-10-24 16:33:33 +0200

    searching subsets simplified

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #include <algorithm>
 #include <cctype>
 #include <locale>
+#include <cmath>
 #include <tuple>
 #include <unordered_set>
 #include "structures.h"
@@ -190,6 +191,13 @@ void load_config()
     return;
 }
 
+int fact(int x)
+{
+    if (x == 1 || x == 0)
+        return 1;
+    return (x * fact(x - 1));
+}
+
 void generate(int k, vector<bool> &my_set, int N, vector<vector<int>> &current_subsets)
 {
     if (k == N)
@@ -223,30 +231,45 @@ vector<vector<int>> generate_subsets(int n)
 
 pair<vector<int>, int> TSP_held_karp()
 {
+    int number_of_combinations = pow(2, number_of_current_graph_vertices - 1);
     vector<vector<int>> subsets = generate_subsets(number_of_current_graph_vertices - 1);
     vector<tuple<pair<int, vector<int>>, int, int>> map_of_costs_and_parents;
     vector<int> path;
-    for (int i = 0; i < subsets.size(); i++) // i numer zbioru
+    int number_of_used_combinations = 0;
+    for (int i = 0; i < number_of_combinations; i++) // i numer zbioru
     {
+        int prev_subs_len = subsets[i].size() - 1;
+        int prepared_combinations = map_of_costs_and_parents.size();
+        int number_of_prev_combinations = 0;
+        if (i > 0)
+        {
+            number_of_prev_combinations = fact(number_of_current_graph_vertices - 1) / (fact(prev_subs_len) * fact(number_of_current_graph_vertices - prev_subs_len - 2));
+            if (subsets[i].size() > subsets[i - 1].size() && i > 1)
+            {
+                number_of_used_combinations = prepared_combinations - number_of_prev_combinations;
+            }
+        }
+
         for (int j = 1; j < number_of_current_graph_vertices; j++) // j numer wierzchołka w grafie (do którego idziemy) wybrany z podzbioru
         {
             vector<int>::iterator it = std::find(subsets[i].begin(), subsets[i].end(), j);
-            if (it == subsets[i].end())
+            if (it == subsets[i].end()) //wykonuj jeśli j nie znajduje się w podzbiorze
             {
                 int local_min_cost = INT_MAX;
                 int parent = 0;
                 for (int k = 0; k < subsets[i].size(); k++)
                 { // k numer elementu w bieżąco analizowanym zbiorze
                     int prev_cost = 0;
-                    for (auto &element : map_of_costs_and_parents)
+                    // for (auto &element : map_of_costs_and_parents)
+                    for (vector<tuple<pair<int, vector<int>>, int, int>>::iterator it = map_of_costs_and_parents.begin() + number_of_used_combinations; it != map_of_costs_and_parents.begin() + number_of_used_combinations + number_of_prev_combinations; ++it)
                     {
                         //znajdź koszt poprzednika
                         vector<int> prev_subset = subsets[i];
                         prev_subset.erase(remove(prev_subset.begin(), prev_subset.end(), subsets[i][k]));
-                        bool subsets_comparsion = std::equal(get<0>(element).second.begin(), get<0>(element).second.end(), prev_subset.begin(), prev_subset.end());
-                        if (get<0>(element).first == subsets[i][k] && subsets_comparsion)
+                        bool subsets_comparsion = std::equal(get<0>(*it).second.begin(), get<0>(*it).second.end(), prev_subset.begin(), prev_subset.end());
+                        if (get<0>(*it).first == subsets[i][k] && subsets_comparsion)
                         {
-                            prev_cost = get<1>(element);
+                            prev_cost = get<1>(*it);
                             break;
                         }
                     }
@@ -261,25 +284,27 @@ pair<vector<int>, int> TSP_held_karp()
                 {
                     local_min_cost = current_graph_adjacency_matrix.matrix[0][j];
                 }
+                if (i == 16)
+                    cout << "";
                 map_of_costs_and_parents.push_back(make_tuple(make_pair(j, subsets[i]), local_min_cost, parent));
             }
         }
     }
-        int final_local_min_cost = INT_MAX;
-        int final_parent = 0;
-        for (auto &element : map_of_costs_and_parents)
+    int final_local_min_cost = INT_MAX;
+    int final_parent = 0;
+    for (auto &element : map_of_costs_and_parents)
+    {
+        if (get<0>(element).second.size() == number_of_current_graph_vertices - 2)
         {
-            if (get<0>(element).second.size() == number_of_current_graph_vertices - 2)
+            int cost = get<1>(element) + current_graph_adjacency_matrix.matrix[get<0>(element).first][0];
+            if (cost < final_local_min_cost)
             {
-                int cost = get<1>(element) + current_graph_adjacency_matrix.matrix[get<0>(element).first][0];
-                if (cost < final_local_min_cost)
-                {
-                    final_local_min_cost = cost;
-                    final_parent = get<0>(element).first;
-                }
+                final_local_min_cost = cost;
+                final_parent = get<0>(element).first;
             }
         }
-        map_of_costs_and_parents.push_back(make_tuple(make_pair(0, subsets[subsets.size() - 1]), final_local_min_cost, final_parent));
+    }
+    map_of_costs_and_parents.push_back(make_tuple(make_pair(0, subsets[subsets.size() - 1]), final_local_min_cost, final_parent));
     path.push_back(0);
     for (int i = 5; i > 0; i--)
     {
@@ -300,7 +325,7 @@ pair<vector<int>, int> TSP_held_karp()
     }
     path.push_back(0);
     int weight = get<1>(map_of_costs_and_parents[map_of_costs_and_parents.size() - 1]);
-    reverse(path.begin(),path.end());
+    std::reverse(path.begin(), path.end());
     return make_pair(path, weight);
 }
 int main()
